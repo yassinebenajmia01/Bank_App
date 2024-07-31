bad82d8b73ff469e30930b3a0fc733e8b8943a0d
yassinebenajmia01 <yacineajmiaskip7530@gmail.com>
2024-07-31 02:13:57 +0100

    31 juillet

--- src/Component/list.js
+++ src/Component/list.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Products from './Db/pdb';
 import Product from '../Component/product';
@@ -8,27 +7,30 @@ function ListProduct() {
     <>
       <h2 className="text-2xl font-bold ml-[25%] h-[100px]">BEST SELLER IN THIS CATEGORY</h2>
       <div className="flex justify-between items-center py-4 border-b">
-      <span className='ml-[25%] font-semibold text-gray-400'><span className='font-extrabold text-black' >1 - 40</span> of 120 results</span>
-      <div className="flex items-center space-x-4 mr-[30%]">
-        <span className='text-gray-400 font-semibold'>Show item:</span>
-        <div className="flex space-x-2 bg-gray-200">
-          <button className="px-3 py-1  text-black font-bold">24</button>
-          <button className="px-3 py-1 font-bold text-gray-500">48</button>
-          <button className="px-3 py-1 font-bold  text-gray-500">72</button>
-        </div>
-        <div className="flex items-center space-x-2 ">
-          <span>Show item:</span>
-          <select className="px-3 py-1 border rounded ml-9">
-            <option value="default">Default</option>
-            <option value="price">Price</option>
-            <option value="name">Name</option>
-          </select>
-        </div>
-        <div className="flex items-center space-x-2">
-          <span className='ml-[800%]'>View<span>As</span></span>
+        <span className='ml-[25%] font-semibold text-gray-400'>
+          <span className='font-extrabold text-black'>1 - 40</span> of 120 results
+        </span>
+        <div className="flex items-center space-x-4 mr-[30%]">
+          <span className='text-gray-400 font-semibold'>Show item:</span>
+          <div className="flex space-x-2 bg-gray-200 ml-[-100px]">
+            <button className="px-3 py-1 text-black font-bold">24</button>
+            <button className="px-3 py-1 font-bold text-gray-500">48</button>
+            <button className="px-3 py-1 font-bold text-gray-500">72</button>
+          </div>
+          <div className="flex items-center space-x-2 ml-8">
+            <span>Show item:</span>
+            <select className="px-3 py-1 border rounded">
+              <option value="default">Default</option>
+              <option value="price">Price</option>
+              <option value="name">Name</option>
+            </select>
+          </div>
+          <div className="flex items-center space-x-2 ml-auto">
+            <span>View</span>
+            <span className="ml-1">As</span>
+          </div>
         </div>
       </div>
-    </div>
       <div className="grid grid-cols-4 gap-1 ml-[25%] w-[70%] h-[1000px]">
         {Products.map((product, index) => (
           <div key={index}>
